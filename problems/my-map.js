2988cd0f18637e79938c2f9b02775911e0000b7c
Srijan Paulsatya <srijanpaulsatya@gmail.com>
2024-11-18 00:48:42 +0530

    Implement Phase 3 | myMap Func

--- problems/my-map.js
+++ problems/my-map.js
@@ -1,5 +1,23 @@
 function myMap(inputArray, callback) {
   // Your code here
+  let newArray = [];
+
+  for (let i = 0; i < inputArray.length; i++) {
+
+    let el = inputArray[i];
+    let callbackResult = callback(el);
+    newArray.push(callbackResult);
+
+  }
+
+  return newArray;
+
 }
+
+let arr = [1,2,3];
+let doubler = (el) => el * 2; 
+
+console.log(arr);
+console.log(myMap(arr, doubler));
 
 module.exports = myMap;
--- test/my-map-spec.js
+++ test/my-map-spec.js
@@ -1,1 +1,44 @@
 // Your code here
+
+const chai = require('chai');
+const spies = require('chai-spies');
+const expect = chai.expect;
+chai.use(spies);
+
+const myMap = require("../problems/my-map.js");
+
+
+describe("myMap(inputArray, callback)", function () {
+
+	let array;
+	let callback;
+
+	beforeEach(() =>  {
+
+		array = [1,2,3]
+		callback = (el) => el * 2;
+
+	});
+
+	it("should map the callback function to each element in inputArray", function () {
+		expect(myMap(array, callback)).to.deep.equal([2,4,6]);
+	});
+
+	it("should not mutate original array", function() {
+		let oldArray = array;
+		const callBackResult = myMap(array,callback);
+		expect(array).to.equal(oldArray);
+	});
+
+	it("should not use the built in Array.map()", function () {
+		const functionSpy = chai.spy.on(array, 'map');
+		expect(functionSpy).to.not.be.called;
+	});
+	
+	it("should invoke the callback for each element in input array", function () {
+		const spy = chai.spy(callback);
+		myMap(array, spy);
+		expect(spy).to.be.called.exactly(array.length);
+	});
+
+});
