5090af26e218d9451943b375d0af85ecc64936b5
Srijan Paulsatya <srijanpaulsatya@gmail.com>
2024-11-17 13:51:08 +0530

    Implement Phase 1

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,3 +1,16 @@
 module.exports = function reverseString(string) {
   // Your code here
+
+  if (typeof string !== 'string') {
+    throw new TypeError("Parameter is not a string!");
+  }
+
+  let reversedWord = "";
+  
+  for(let i = string.length - 1; i >= 0; i--) {
+    reversedWord += string[i];
+  }
+
+  return reversedWord;
+
 };
--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,21 @@
 // Your code here
+
+const { expect } = require('chai');
+
+const reverseString = require('../problems/reverse-string.js');
+
+
+describe('Reverse String', function () {
+
+	it('should reverse the given string', function () {
+
+		let string = "fun";
+		let reversed = reverseString(string);
+		expect(reversed).to.equal("nuf");
+	});
+
+	it('should throw an error if input is not a string', function () {
+		expect(() => reverseString(3)).to.throw();
+	});
+
+});
