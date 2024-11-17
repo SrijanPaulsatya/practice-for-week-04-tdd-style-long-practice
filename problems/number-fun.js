e23dedb359cd65c713633002b7b46b17637a83dd
Srijan Paulsatya <srijanpaulsatya@gmail.com>
2024-11-17 14:24:09 +0530

    Implement Phase 2

--- problems/number-fun.js
+++ problems/number-fun.js
@@ -1,9 +1,16 @@
 function returnsThree() {
   // Your code here
+  return 3;
 }
 
 function reciprocal(n) {
   // Your code here
+
+  if(n <= 0 || n > 1000000) {
+    throw new TypeError("Parameter can't be less than 0 or greater than 1000000");
+  }
+
+  return (1 / n);
 }
 
 module.exports = {
--- test/number-fun-spec.js
+++ test/number-fun-spec.js
@@ -1,1 +1,37 @@
 // Your code here
+
+const chai = require('chai');
+const expect = chai.expect;
+
+const { returnsThree, reciprocal } = require('../problems/number-fun.js');
+
+
+describe("Returns Three", function () {
+
+	it('should return true if number is 3', function () {
+		expect(returnsThree()).to.equal(3);
+	});
+
+});
+
+describe("Reciprocal", function () {
+	
+	it('should return the reciprocal of the number', function () {
+		let n = 4;
+		expect(reciprocal(n)).to.equal(0.25);
+	});
+
+	it('should return the reciprocal of the number (if it\'s a fraction)', function () {
+		expect(reciprocal(0.25)).to.equal(4);
+	});
+
+	it('should throw an error if input less than 0 or greater than 1000000', function () {
+		expect(() => reciprocal(0)).to.throw();
+	});
+
+	it('should throw an error if input less than 0 or greater than 1000000', function () {
+		expect(() => reciprocal(100000000)).to.throw();
+	});
+
+});
+
